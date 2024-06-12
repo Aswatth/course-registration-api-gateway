fd56c8b9ad622734a9057d1b1882c7c58eae1c2e
unknown <aswatth56@gmail.com>
2024-06-12 20:48:22 +0530

    Added package to read .env files and basic REST API setup

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run(":" + os.Getenv("PORT"))
 }
