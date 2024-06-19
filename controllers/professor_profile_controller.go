1d5f9f203b9b770b4ab56e43bd3384e7e6c92fe6
unknown <aswatth56@gmail.com>
2024-06-19 17:42:02 +0530

    Added CRUD operations for offered_courses

--- controllers/professor_profile_controller.go
+++ controllers/professor_profile_controller.go
@@ -3,6 +3,8 @@ package controllers
 import (
 	"course-registration-system/api-gateway/middlewares"
 	"course-registration-system/api-gateway/services"
+	"encoding/json"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -19,10 +21,41 @@ func (obj *ProfessorProfileController) UpdateProfessorPassword(context *gin.Cont
 	obj.service.UpdateProfessorPassword(context)
 }
 
+func (obj *ProfessorProfileController) OfferCourse(context *gin.Context) {
+	obj.service.OfferCourse(context)
+}
+
+func (obj *ProfessorProfileController) GetOfferedCourse(context *gin.Context) {
+	result, err := obj.service.GetOfferedCourse(context)
+
+	if err != nil {
+		context.AbortWithStatusJSON(http.StatusBadRequest, err)
+	}
+
+	var data interface{}
+
+	json.Unmarshal(result, &data)
+
+	context.JSON(http.StatusOK, data)
+}
+
+func (obj *ProfessorProfileController) UpdateOfferedCourse(context *gin.Context) {
+	obj.service.UpdateOfferedCourse(context)
+}
+
+func (obj *ProfessorProfileController) DeleteOfferedCourse(context *gin.Context) {
+	obj.service.DeleteOfferedCourse(context)
+}
+
 func (obj *ProfessorProfileController) RegisterRoutes(rg *gin.RouterGroup) {
 	professor_routes := rg.Group("/professors")
 
 	professor_routes.Use(middlewares.ValidateAuthorization([]string{"PROFESSOR"}))
 
 	professor_routes.PUT("/password/:email_id", obj.UpdateProfessorPassword)
+
+	professor_routes.POST("/offered_course", obj.OfferCourse)
+	professor_routes.GET("/offered_course/:crn", obj.GetOfferedCourse)
+	professor_routes.PUT("/offered_course/:crn", obj.UpdateOfferedCourse)
+	professor_routes.DELETE("/offered_course/:crn", obj.DeleteOfferedCourse)
 }
--- services/professor_profile_service.go
+++ services/professor_profile_service.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"io"
 	"net/http"
 	"os"
 
@@ -17,6 +18,42 @@ func (obj *ProfessorProfileService) Init() {
 
 func (obj *ProfessorProfileService) UpdateProfessorPassword(context *gin.Context) {
 	req, _ := http.NewRequest("PUT", os.Getenv("PROFILE_SERVICE")+"/professors/password/"+context.Param("email_id"), context.Request.Body)
+
+	req.Header.Set("Authorization", context.Request.Header.Get("Authorization"))
+
+	obj.client.Do(req)
+}
+
+func (obj *ProfessorProfileService) OfferCourse(context *gin.Context) {
+	req, _ := http.NewRequest("POST", os.Getenv("REGISTRATION_SERVICE")+"/offered_course", context.Request.Body)
+
+	req.Header.Set("Authorization", context.Request.Header.Get("Authorization"))
+
+	obj.client.Do(req)
+}
+
+func (obj *ProfessorProfileService) GetOfferedCourse(context *gin.Context) ([]byte, error) {
+	req, _ := http.NewRequest("GET", os.Getenv("REGISTRATION_SERVICE")+"/offered_course/"+context.Param("crn"), context.Request.Body)
+
+	req.Header.Set("Authorization", context.Request.Header.Get("Authorization"))
+
+	response, _ := obj.client.Do(req)
+
+	result, err := io.ReadAll(response.Body)
+
+	return result, err
+}
+
+func (obj *ProfessorProfileService) UpdateOfferedCourse(context *gin.Context) {
+	req, _ := http.NewRequest("PUT", os.Getenv("REGISTRATION_SERVICE")+"/offered_course/"+context.Param("crn"), context.Request.Body)
+
+	req.Header.Set("Authorization", context.Request.Header.Get("Authorization"))
+
+	obj.client.Do(req)
+}
+
+func (obj *ProfessorProfileService) DeleteOfferedCourse(context *gin.Context) {
+	req, _ := http.NewRequest("DELETE", os.Getenv("REGISTRATION_SERVICE")+"/offered_course/"+context.Param("crn"), context.Request.Body)
 
 	req.Header.Set("Authorization", context.Request.Header.Get("Authorization"))
 
