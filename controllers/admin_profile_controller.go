2ab1c792f295c67b7a09f87134ca5c560f475725
Aswatth <aswatth56@gmail.com>
2024-07-03 11:26:10 +0530

    Added error handling to admin_profile_service

--- controllers/admin_profile_controller.go
+++ controllers/admin_profile_controller.go
@@ -3,8 +3,6 @@ package controllers
 import (
 	"course-registration-system/api-gateway/middlewares"
 	"course-registration-system/api-gateway/services"
-	"encoding/json"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -18,89 +16,35 @@ func (obj *AdminProfileController) Init(service services.AdminProfileService) {
 }
 
 func (obj *AdminProfileController) CreateStudent(context *gin.Context) {
-	obj.service.CreateStudentProfile(context)
+	obj.service.StudentProfileActions("POST", context)
 }
 
 func (obj *AdminProfileController) GetStudentProfile(context *gin.Context) {
-	result, err := obj.service.GetStudentProfile(context)
-
-	if err != nil {
-		context.AbortWithError(http.StatusBadGateway, err)
-	}
-
-	var data interface{}
-	json.Unmarshal(result, &data)
-
-	context.JSON(http.StatusOK, data)
-}
-
-func (obj *AdminProfileController) GetAllStudentProfiles(context *gin.Context) {
-	if(context.Query("email_id") != "") {
-		obj.GetStudentProfile(context)
-		return
-	}
-
-	result, err := obj.service.GetStudentProfile(context)
-
-	if err != nil {
-		context.AbortWithError(http.StatusBadGateway, err)
-	}
-
-	var data interface{}
-	json.Unmarshal(result, &data)
-
-	context.JSON(http.StatusOK, data)
+	obj.service.StudentProfileActions("GET", context)
 }
 
 func (obj *AdminProfileController) UpdateStudent(context *gin.Context) {
-	obj.service.UpdateStudentProfile(context)
+	obj.service.StudentProfileActions("PUT", context)
 }
 
 func (obj *AdminProfileController) DeleteStudent(context *gin.Context) {
-	obj.service.DeleteStudentProfile(context)
+	obj.service.StudentProfileActions("DELETE", context)
 }
 
 func (obj *AdminProfileController) CreateProfessor(context *gin.Context) {
-	obj.service.CreateProfessorProfile(context)
+	obj.service.ProfessorProfileActions("POST", context)
 }
 
 func (obj *AdminProfileController) GetProfessorProfile(context *gin.Context) {
-	result, err := obj.service.GetProfessorProfile(context)
-
-	if err != nil {
-		context.AbortWithError(http.StatusBadGateway, err)
-	}
-
-	m := make(map[string]string)
-	json.Unmarshal(result, &m)
-
-	context.JSON(http.StatusOK, m)
-}
-
-func (obj *AdminProfileController) GetAllProfessorProfiles(context *gin.Context) {
-	if(context.Query("email_id") != "") {
-		obj.GetProfessorProfile(context)
-		return
-	}
-
-	result, err := obj.service.GetAllProfessorProfiles(context)
-
-	if err != nil {
-		context.AbortWithError(http.StatusBadGateway, err)
-	}
-
-	var data interface{}
-	json.Unmarshal(result, &data)
-
-	context.JSON(http.StatusOK, data)
+	obj.service.ProfessorProfileActions("GET", context)
 }
 
 func (obj *AdminProfileController) UpdateProfessor(context *gin.Context) {
-	obj.service.UpdateProfessorProfile(context)
+	obj.service.ProfessorProfileActions("PUT", context)
 }
 
 func (obj *AdminProfileController) DeleteProfessor(context *gin.Context) {
-	obj.service.DeleteProfessorProfile(context)
+	obj.service.ProfessorProfileActions("DELETE", context)
 }
 
 func (obj *AdminProfileController) UpdatePassword(context *gin.Context) {
@@ -113,12 +57,12 @@ func (obj *AdminProfileController) RegisterRoutes(rg *gin.RouterGroup) {
 	admin_routes.Use(middlewares.ValidateAuthorization([]string{"ADMIN"}))
 
 	admin_routes.POST("/students", obj.CreateStudent)
-	admin_routes.GET("/students", obj.GetAllStudentProfiles)
+	admin_routes.GET("/students", obj.GetStudentProfile)
 	admin_routes.DELETE("/students/:email_id", obj.DeleteStudent)
 	admin_routes.PUT("/students/:email_id", obj.UpdateStudent)
 
 	admin_routes.POST("/professors", obj.CreateProfessor)
-	admin_routes.GET("/professors", obj.GetAllProfessorProfiles)
+	admin_routes.GET("/professors", obj.GetProfessorProfile)
 	admin_routes.DELETE("/professors/:email_id", obj.DeleteProfessor)
 	admin_routes.PUT("/professors/:email_id", obj.UpdateProfessor)
 
--- services/admin_profile_service.go
+++ services/admin_profile_service.go
@@ -1,9 +1,12 @@
 package services
 
 import (
+	"encoding/json"
 	"io"
+	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,98 +19,188 @@ func (obj *AdminProfileService) Init() {
 	obj.client = http.Client{}
 }
 
-func (obj *AdminProfileService) CreateStudentProfile(context *gin.Context) {
-	req, _ := http.NewRequest("POST", os.Getenv("PROFILE_SERVICE")+"/admin/students", context.Request.Body)
+func (obj *AdminProfileService) studentGetAction(fetch_all bool, context *gin.Context) {
+	url := ""
+	if(fetch_all) {
+		url = os.Getenv("PROFILE_SERVICE")+"/admin/students"
+	} else {
+		url = os.Getenv("PROFILE_SERVICE")+"/admin/students?email_id="+context.Query("email_id")
+	}
 
-	req.Header.Set("Authorization", context.Request.Header.Get("Authorization"))
+	req, err := http.NewRequest("GET", url, context.Request.Body)
 
-	obj.client.Do(req)
+	if err != nil {
+		log.Println(err.Error())
+		context.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"response": "error creating a new request"})
+	} else {
+		req.Header.Set("Authorization", context.Request.Header.Get("Authorization"))
+
+		response, err := obj.client.Do(req)
+
+		if err != nil {
+			log.Println(err.Error())
+			context.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"response": "error executing a new request"})
+		} else {
+			var data interface{}
+
+			body, _ := io.ReadAll(response.Body)
+
+			json.Unmarshal(body, &data)
+
+			context.JSON(response.StatusCode, data)
+		}
+	}
+}
+func (obj *AdminProfileService) StudentProfileActions(action string, context *gin.Context) {
+	action = strings.ToUpper(action)
+	
+	var url = os.Getenv("PROFILE_SERVICE")+"/admin/students"
+
+	switch action {
+	case "GET" : 
+		if(context.Query("email_id") != "") {
+			obj.studentGetAction(false, context)
+		} else {
+			obj.studentGetAction(true, context)
+		}
+	case "POST": 
+	case "PUT": fallthrough
+	case "DELETE": {url += "/"+context.Param("email_id")}
+	}
+
+	req, err := http.NewRequest(action, url, context.Request.Body)
+
+	if err != nil {
+		log.Println(err.Error())
+		context.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"response": "error creating a new request"})
+	} else {
+		req.Header.Set("Authorization", context.Request.Header.Get("Authorization"))
+
+		response, err := obj.client.Do(req)
+
+		if (err != nil) {
+			log.Println(err.Error())
+			context.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"response": "error executing a new request"})
+		} else {
+			if(response.StatusCode == 200) {
+				context.Status(http.StatusOK)
+			} else {
+				var data interface{}
+
+				body, _ := io.ReadAll(response.Body)
+
+				json.Unmarshal(body, &data)
+
+				context.JSON(response.StatusCode, data)
+			}
+		}
+	}
 }
 
-func (obj *AdminProfileService) GetStudentProfile(context *gin.Context) ([]byte, error) {
-	req, _ := http.NewRequest("GET", os.Getenv("PROFILE_SERVICE")+"/admin/students?email_id="+context.Query("email_id"), context.Request.Body)
+func (obj *AdminProfileService) professorGetAction(fetch_all bool, context *gin.Context) {
+	url := ""
+	if(fetch_all) {
+		url = os.Getenv("PROFILE_SERVICE")+"/admin/professors"
+	} else {
+		url = os.Getenv("PROFILE_SERVICE")+"/admin/professors?email_id="+context.Query("email_id")
+	}
 
-	req.Header.Set("Authorization", context.Request.Header.Get("Authorization"))
+	req, err := http.NewRequest("GET", url, context.Request.Body)
 
-	response, _ := obj.client.Do(req)
+	if err != nil {
+		log.Println(err.Error())
+		context.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"response": "error creating a new request"})
+	} else {
+		req.Header.Set("Authorization", context.Request.Header.Get("Authorization"))
 
-	return io.ReadAll(response.Body)
+		response, err := obj.client.Do(req)
+
+		if err != nil {
+			log.Println(err.Error())
+			context.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"response": "error executing a new request"})
+		} else {
+			var data interface{}
+
+			body, _ := io.ReadAll(response.Body)
+
+			json.Unmarshal(body, &data)
+
+			context.JSON(response.StatusCode, data)
+		}
+	}
 }
+func (obj *AdminProfileService) ProfessorProfileActions(action string, context *gin.Context) {
+	action = strings.ToUpper(action)
+	
+	var url = os.Getenv("PROFILE_SERVICE")+"/admin/professors"
 
-func (obj *AdminProfileService) GetAllStudentProfiles(context *gin.Context) ([]byte, error) {
-	req, _ := http.NewRequest("GET", os.Getenv("PROFILE_SERVICE")+"/admin/students", context.Request.Body)
+	switch action {
+	case "GET" : 
+		if(context.Query("email_id") != "") {
+			obj.professorGetAction(false, context)
+		} else {
+			obj.professorGetAction(true, context)
+		}
+	case "POST": 
+	case "PUT": fallthrough
+	case "DELETE": {url += "/"+context.Param("email_id")}
+	}
 
-	req.Header.Set("Authorization", context.Request.Header.Get("Authorization"))
+	req, err := http.NewRequest(action, url, context.Request.Body)
 
-	response, _ := obj.client.Do(req)
+	if err != nil {
+		log.Println(err.Error())
+		context.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"response": "error creating a new request"})
+	} else {
+		req.Header.Set("Authorization", context.Request.Header.Get("Authorization"))
 
-	return io.ReadAll(response.Body)
-}
+		response, err := obj.client.Do(req)
 
-func (obj *AdminProfileService) UpdateStudentProfile(context *gin.Context) {
-	req, _ := http.NewRequest("PUT", os.Getenv("PROFILE_SERVICE")+"/admin/students/"+context.Param("email_id"), context.Request.Body)
+		if (err != nil) {
+			log.Println(err.Error())
+			context.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"response": "error executing a new request"})
+		} else {
+			if(response.StatusCode == 200) {
+				context.Status(http.StatusOK)
+			} else {
+				var data interface{}
 
-	req.Header.Set("Authorization", context.Request.Header.Get("Authorization"))
+				body, _ := io.ReadAll(response.Body)
 
-	obj.client.Do(req)
-}
+				json.Unmarshal(body, &data)
 
-func (obj *AdminProfileService) DeleteStudentProfile(context *gin.Context) {
-	req, _ := http.NewRequest("DELETE", os.Getenv("PROFILE_SERVICE")+"/admin/students/"+context.Param("email_id"), context.Request.Body)
-
-	req.Header.Set("Authorization", context.Request.Header.Get("Authorization"))
-
-	obj.client.Do(req)
-}
-
-func (obj *AdminProfileService) CreateProfessorProfile(context *gin.Context) {
-	req, _ := http.NewRequest("POST", os.Getenv("PROFILE_SERVICE")+"/admin/professors", context.Request.Body)
-
-	req.Header.Set("Authorization", context.Request.Header.Get("Authorization"))
-
-	obj.client.Do(req)
-}
-
-func (obj *AdminProfileService) GetProfessorProfile(context *gin.Context) ([]byte, error) {
-	req, _ := http.NewRequest("GET", os.Getenv("PROFILE_SERVICE")+"/admin/professors?email_id="+context.Query("email_id"), context.Request.Body)
-
-	req.Header.Set("Authorization", context.Request.Header.Get("Authorization"))
-
-	response, _ := obj.client.Do(req)
-
-	return io.ReadAll(response.Body)
-}
-
-func (obj *AdminProfileService) GetAllProfessorProfiles(context *gin.Context) ([]byte, error) {
-	req, _ := http.NewRequest("GET", os.Getenv("PROFILE_SERVICE")+"/admin/professors", context.Request.Body)
-
-	req.Header.Set("Authorization", context.Request.Header.Get("Authorization"))
-
-	response, _ := obj.client.Do(req)
-
-	return io.ReadAll(response.Body)
-}
-
-func (obj *AdminProfileService) UpdateProfessorProfile(context *gin.Context) {
-	req, _ := http.NewRequest("PUT", os.Getenv("PROFILE_SERVICE")+"/admin/professors/"+context.Param("email_id"), context.Request.Body)
-
-	req.Header.Set("Authorization", context.Request.Header.Get("Authorization"))
-
-	obj.client.Do(req)
-}
-
-func (obj *AdminProfileService) DeleteProfessorProfile(context *gin.Context) {
-	req, _ := http.NewRequest("DELETE", os.Getenv("PROFILE_SERVICE")+"/admin/professors/"+context.Param("email_id"), context.Request.Body)
-
-	req.Header.Set("Authorization", context.Request.Header.Get("Authorization"))
-
-	obj.client.Do(req)
+				context.JSON(response.StatusCode, data)
+			}
+		}
+	}
 }
 
 func (obj *AdminProfileService) UpdatePassword(context *gin.Context) {
-	req, _ := http.NewRequest("PUT", os.Getenv("PROFILE_SERVICE")+"/admin/password/"+context.Param("email_id"), context.Request.Body)
+	req, err := http.NewRequest("PUT", os.Getenv("PROFILE_SERVICE")+"/admin/password/"+context.Param("email_id"), context.Request.Body)
 
-	req.Header.Set("Authorization", context.Request.Header.Get("Authorization"))
+	if err != nil {
+		log.Println(err.Error())
+		context.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"response": "error creating a new request"})
+	} else {
+		req.Header.Set("Authorization", context.Request.Header.Get("Authorization"))
 
-	obj.client.Do(req)
+		response, err := obj.client.Do(req)
+
+		if (err != nil) {
+			log.Println(err.Error())
+			context.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"response": "error executing a new request"})
+		} else {
+			if(response.StatusCode == 200) {
+				context.Status(http.StatusOK)
+			} else {
+				var data interface{}
+
+				body, _ := io.ReadAll(response.Body)
+
+				json.Unmarshal(body, &data)
+
+				context.JSON(response.StatusCode, data)
+			}
+		}
+	}
 }
